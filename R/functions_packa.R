2d8c63bce2ea9706c22661b12b5f9543f7253180
Stephane <sdeboudt@hotmail.com>
2017-09-13 15:54:42 +0200

    Initial Commit

---
+++ R/functions_packa.R
@@ -0,0 +1,12 @@
+
+#' @title f1 exposed by packa
+#' @description This is function \code{f1} exposed by \code{packa}
+#'
+#' @return A String "Hello from packa_f1"
+#' @export
+#'
+#' @examples
+#' packa_f1()
+packa_f1 <- function() {
+  print("Hello from packa_f1")
+}
